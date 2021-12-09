25b6c46101adaf81ec5551987ada4ac1469379ee
sbelzile <seb.belzile@gmail.com>
2021-12-09 18:12:50 -0500

    part 3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 mod camera;
 pub use camera::*;
 
@@ -14,8 +15,10 @@ fn main() {
             ..Default::default()
         })
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_startup_system(setup.system())
         .add_startup_stage("player_setup", SystemStage::single(spawn_player.system()))
+        .add_startup_stage("floor_setup", SystemStage::single(spawn_floor.system()))
         .add_plugins(DefaultPlugins)
         .run();
 }
@@ -25,11 +28,51 @@ fn setup(mut commands: Commands) {
 }
 
 fn spawn_player(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    let rigid_body = RigidBodyBundle {
+        mass_properties: RigidBodyMassPropsFlags::ROTATION_LOCKED.into(),
+        activation: RigidBodyActivation::cannot_sleep(),
+        ccd: RigidBodyCcd { ccd_enabled: true, ..Default::default() },
+        ..Default::default()
+    };
+    let collider = ColliderBundle {
+        shape: ColliderShape::cuboid(0.5, 0.5),
+        flags: ColliderFlags {
+            active_events: ActiveEvents::CONTACT_EVENTS,
+            ..Default::default()
+        },
+        ..Default::default()
+    };
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
             sprite: Sprite::new(Vec2::new(1.0, 1.0)),
             ..Default::default()
         })
+        .insert_bundle(rigid_body)
+        .insert_bundle(collider)
+        .insert(RigidBodyPositionSync::Discrete)
         .insert(Player);
+}
+
+fn spawn_floor(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    let width = 10.;
+    let height = 1.;
+    let rigid_body = RigidBodyBundle {
+        position: Vec2::new(0.0, -2.).into(),
+        body_type: RigidBodyType::Static,
+        ..Default::default()
+    };
+    let collider = ColliderBundle {
+        shape: ColliderShape::cuboid(width / 2., height / 2.),
+        ..Default::default()
+    };
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+            sprite: Sprite::new(Vec2::new(width, height)),
+            ..Default::default()
+        })
+        .insert_bundle(rigid_body)
+        .insert_bundle(collider)
+        .insert(RigidBodyPositionSync::Discrete);
 }
