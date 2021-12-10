baa35c74aa6316126ca57296fc3d089e9b346a80
sbelzile <seb.belzile@gmail.com>
2021-12-09 19:15:23 -0500

    part 4: jumps

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ pub use camera::*;
 
 struct Player;
 
+struct Jumper {
+    jump_impulse: f32,
+    is_jumping: bool
+}
+
 fn main() {
     App::build()
         .insert_resource(WindowDescriptor {
@@ -19,6 +24,8 @@ fn main() {
         .add_startup_system(setup.system())
         .add_startup_stage("player_setup", SystemStage::single(spawn_player.system()))
         .add_startup_stage("floor_setup", SystemStage::single(spawn_floor.system()))
+        .add_system(player_jumps.system())
+        .add_system(jump_reset.system())
         .add_plugins(DefaultPlugins)
         .run();
 }
@@ -51,7 +58,20 @@ fn spawn_player(mut commands: Commands, mut materials: ResMut<Assets<ColorMateri
         .insert_bundle(rigid_body)
         .insert_bundle(collider)
         .insert(RigidBodyPositionSync::Discrete)
-        .insert(Player);
+        .insert(Player)
+        .insert(Jumper { jump_impulse: 10., is_jumping: false });
+}
+
+fn player_jumps(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut players: Query<(&mut Jumper, &mut RigidBodyVelocity), With<Player>>
+) {
+    for (mut jumper, mut velocity) in players.iter_mut() {
+        if keyboard_input.pressed(KeyCode::Up) && !jumper.is_jumping {
+            velocity.linvel = Vec2::new(0., jumper.jump_impulse).into();
+            jumper.is_jumping = true
+        }
+    }
 }
 
 fn spawn_floor(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
@@ -75,4 +95,19 @@ fn spawn_floor(mut commands: Commands, mut materials: ResMut<Assets<ColorMateria
         .insert_bundle(rigid_body)
         .insert_bundle(collider)
         .insert(RigidBodyPositionSync::Discrete);
+}
+
+fn jump_reset(
+    mut query: Query<(Entity, &mut Jumper)>,
+    mut contact_events: EventReader<ContactEvent>,
+) {
+    for contact_event in contact_events.iter() {
+        for (entity, mut jumper) in query.iter_mut() {
+            if let ContactEvent::Started(h1, h2) = contact_event {
+                if h1.entity() == entity || h2.entity() == entity {
+                    jumper.is_jumping = false
+                }
+            }
+        }
+    }
 }
