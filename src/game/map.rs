0fbc2b690f3972774b38a89cb43fce727436f689
Sébastien Belzile <67745993+sbelzile-nexapp@users.noreply.github.com>
2021-12-19 12:04:11 -0500

    feat: death & enemies (#5)
    * feat: death & enemies
    
    * don't die so low
    
    Co-authored-by: sbelzile <seb.belzile@gmail.com>

--- src/game/map.rs
+++ src/game/map.rs
@@ -1,4 +1,4 @@
-use super::components::Materials;
+use super::{components::Materials, Enemy, insert_monster_at};
 use bevy::{ecs::system::EntityCommands, prelude::*};
 use bevy_rapier2d::prelude::*;
 use rand::prelude::*;
@@ -10,21 +10,49 @@ pub struct MapData {
 pub fn spawn_floor(mut commands: Commands, materials: Res<Materials>) {
     let mut map_parent = commands.spawn();
     let world = create_world(150);
-    add_sprites(&mut map_parent, materials, &world);
+    add_sprites(&mut map_parent, &materials, &world);
     add_colliders(&world, &mut map_parent);
-
     let map_entity = map_parent.id();
     commands.insert_resource(MapData { map_entity });
+
+    add_enemies(&mut commands, &world, &materials);
 }
 
-pub fn cleanup_map(mut commands: Commands, map_data: Res<MapData>) {
+pub fn cleanup_map(
+    mut commands: Commands,
+    map_data: Res<MapData>,
+    monsters: Query<Entity, With<Enemy>>,
+) {
     commands.entity(map_data.map_entity).despawn_recursive();
+    for monster in monsters.iter() {
+        commands.entity(monster).despawn_recursive();
+    }
 }
 
-fn add_sprites(commands: &mut EntityCommands, materials: Res<Materials>, world: &Vec<usize>) {
+fn add_sprites(commands: &mut EntityCommands, materials: &Res<Materials>, world: &Vec<usize>) {
     world.iter().enumerate().for_each(|(x, height)| {
-        add_tile(commands, &materials, x as f32, *height);
+        add_tile(commands, materials, x as f32, *height);
     });
+}
+
+fn add_enemies(commands: &mut Commands, world: &Vec<usize>, materials: &Res<Materials>) {
+    world.iter().enumerate().for_each(|(x, height)| {
+        if should_add_enemy(x) {
+            insert_monster_at(commands, x, *height + 1, materials)
+        }
+    })
+}
+
+fn should_add_enemy(x: usize) -> bool {
+    if x <= 5 {
+        return false;
+    }
+    let mut rng = thread_rng();
+    let random_number: u32 = rng.gen_range(0..100);
+    match random_number {
+        0..=90 => false,
+        _ => true,
+    }
 }
 
 fn create_world(width: usize) -> Vec<usize> {
@@ -65,7 +93,7 @@ fn add_tile(commands: &mut EntityCommands, materials: &Res<Materials>, x: f32, h
             sprite: Sprite::new(Vec2::new(1., height as f32)),
             global_transform: GlobalTransform::from_translation(Vec3::new(
                 x,
-                height as f32 / 2.,
+                height as f32 / 2. + 0.5,
                 0.,
             )),
             ..Default::default()
@@ -102,7 +130,7 @@ fn add_collider(commands: &mut EntityCommands, height: usize, from: usize, to: u
     let width = to - from;
     let half_width = width as f32 / 2.;
     let rigid_body = RigidBodyBundle {
-        position: Vec2::new(from as f32 + half_width - 0.5, height as f32 - 0.5).into(),
+        position: Vec2::new(from as f32 + half_width - 0.5, height as f32).into(),
         body_type: RigidBodyType::Static,
         ..Default::default()
     };
